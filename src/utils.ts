b679c2ceb7fe4921398aedc748bdf3540e1db03f
Amovane <cybermageek@outlook.com>
2023-12-22 23:27:34 +0800

    chore: remove useless dependencies

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,3 @@
-import fs from "fs";
-import yaml from 'js-yaml';
-
 export function delay(mills: number) {
   return new Promise(resolve => setTimeout(resolve, mills));
-}
-
-export function readYaml<T>(filepath: string): T {
-  return yaml.load(fs.readFileSync(filepath, "utf8")) as T
 }
