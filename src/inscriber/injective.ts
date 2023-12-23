e06608bdfd072bf44dc1d6ceb1c00949a62d99b6
Amovane <cybermageek@outlook.com>
2023-12-23 13:44:32 +0800

    optimize(injective): make gas configurable

--- src/inscriber/injective.ts
+++ src/inscriber/injective.ts
@@ -2,8 +2,11 @@ import { CosmosConfig, TxRequest } from "./inscriber";
 import { BaseAccount, ChainRestAuthApi, MsgSend, PrivateKey, TxGrpcClient, createTransaction } from "@injectivelabs/sdk-ts";
 import { Network, getNetworkInfo } from "@injectivelabs/networks";
 import { CosmosInscriber } from "./cosmos";
+import { StdFee } from "@cosmjs/amino";
+import { BigNumberInWei } from "@injectivelabs/utils";
 
 export class InjectiveInscriber extends CosmosInscriber {
+  MINIMUM_GAS_PRICE = new BigNumberInWei('500000000');
   constructor(config: CosmosConfig) {
     super(config);
   }
@@ -20,9 +23,8 @@ export class InjectiveInscriber extends CosmosInscriber {
           denom: "inj",
         };
         const publicKey = signer.toPublicKey().toBase64();
-        const accountDetailsResponse = await new ChainRestAuthApi(
-          network.rest
-        ).fetchAccount(sender);
+        const authApi = new ChainRestAuthApi(network.rest);
+        const accountDetailsResponse = await authApi.fetchAccount(sender);
         const baseAccount = BaseAccount.fromRestApi(accountDetailsResponse);
         const accountDetails = baseAccount.toAccountDetails();
         const msg = MsgSend.fromJSON({
@@ -30,25 +32,46 @@ export class InjectiveInscriber extends CosmosInscriber {
           srcInjectiveAddress: sender,
           dstInjectiveAddress: recipient,
         });
-        // FIXME: configurable gas 
-        const { signBytes, txRaw } = createTransaction({
+        let txArgs = {
           message: msg,
           memo: memo.toString(),
-          fee: {
-            amount: [
-              {
-                amount: '2000000000000000',
-                denom: "inj",
-              },
-            ],
-            gas: "900000",
-          },
           pubKey: publicKey,
           sequence: accountDetails.sequence,
           accountNumber: accountDetails.accountNumber,
           chainId: network.chainId,
-        });
+        }
 
+        const gasOverrided = this.config.gasLimit;
+        let gasLimit: string;
+        let gasPrice: string;
+        let fee: StdFee
+        if (gasOverrided) {
+          gasPrice = this.config.gasPrice?.toString() ?? this.MINIMUM_GAS_PRICE.toFixed();
+          gasPrice = this.MINIMUM_GAS_PRICE.gt(new BigNumberInWei(gasPrice)) ? this.MINIMUM_GAS_PRICE.toFixed() :
+            gasPrice;
+          gasLimit = this.config.gasLimit!.toString();
+        } else {
+          // simulate 
+          const { signBytes, txRaw } = createTransaction(txArgs);
+          const signature = await signer.sign(Buffer.from(signBytes));
+          txRaw.signatures = [signature];
+          const txService = new TxGrpcClient(network.grpc);
+          const { gasInfo } = await txService.simulate(txRaw)
+          gasPrice = this.MINIMUM_GAS_PRICE.toFixed();
+          gasLimit = gasInfo.gasWanted!.toString();
+        }
+        fee = {
+          amount: [
+            {
+              amount: gasPrice,
+              denom: "inj",
+            },
+          ],
+          gas: gasLimit,
+        };
+
+        // broadcast
+        const { signBytes, txRaw } = createTransaction(txArgs);
         const signature = await signer.sign(Buffer.from(signBytes));
         txRaw.signatures = [signature];
         const txService = new TxGrpcClient(network.grpc);
